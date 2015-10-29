065b1ef7a63a7738e9a3ea565b9c8db053f0d082
CVeiga <krlosva82@gmail.com>
2015-10-29 23:15:49 +0100

    New actionlib for search object

--- pan_tilt_object_localization/src/find_object_client.cpp
+++ pan_tilt_object_localization/src/find_object_client.cpp
@@ -1,27 +1,81 @@
-#include "ros/ros.h"
-#include "pan_tilt_object_localization/FindAnObject.h"
-#include <cstdlib>
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <pan_tilt_object_localization/FindAnObjectAction.h>
 
-int main(int argc, char **argv)
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp> 
+
+#include "std_msgs/Float32MultiArray.h"
+
+std::vector<cv::Point2f> _scene_corners(4);
+uint _direction = 1;
+uint _cont = 0;
+
+
+bool dimmensionBox() { 
+    if(abs(_scene_corners[0].x - _scene_corners[1].x) * abs(_scene_corners[1].y - _scene_corners[2].y) > 800) 
+    { 
+        _direction = 0; 
+        return true;
+    } 
+    else return false;
+}
+
+void paintBox(const std_msgs::Float32MultiArray& points)
 {
-   ros::init(argc, argv, "find_object_client");
-   if (argc != 2)
-   {
-     ROS_INFO("usage: find_object_server T");
-     return 1;
-   }
-   
-   ros::NodeHandle n;
-   ros::ServiceClient client = n.serviceClient<pan_tilt_object_localization::FindAnObject>("find_time");
-   pan_tilt_object_localization::FindAnObject srv;
-   srv.request.time = atoll(argv[1]);
+    std::cout << "hola" << std::endl;
 
-   if (client.call(srv)) ROS_INFO("Object LOCATED");
-   else
-   {
-     ROS_ERROR("Object NOT LOCATED");
-     return 1;
-   }
-  
-   return 0;
+    actionlib::SimpleActionClient<pan_tilt_object_localization::FindAnObjectAction> ac("find_object", true);
+
+    for (int i = 0; i < 4; i++)
+    {
+        _scene_corners[i].x = points.data[i*2];
+        _scene_corners[i].y = points.data[i*2+1];
+    }
+    dimmensionBox();
+
+    //ROS_INFO("Waiting for action server to start.");
+    // wait for the action server to start
+    if(_cont % 5 == 0)
+    {
+        ac.waitForServer(); //will wait for infinite time
+
+        ROS_INFO("Action server started, sending goal.");
+        // send a goal to the action
+        pan_tilt_object_localization::FindAnObjectGoal goal;
+        goal.direction = _direction;
+        ac.sendGoal(goal);
+
+        //wait for the action to return
+        bool finished_before_timeout = ac.waitForResult(ros::Duration(2.0));    //5 is good
+
+          
+        actionlib::SimpleClientGoalState state = ac.getState();
+        if (finished_before_timeout)
+        {
+            ROS_INFO("Action finished: %s",state.toString().c_str());
+            if (_direction == 0) exit(0);
+            //if (state.toString().c_str() == "SUCCEEDED") exit(0);
+        }
+        else
+        {
+            ROS_INFO("Object Not Found.");
+            std::cout << "GETSTATE: " << state.toString().c_str() << std::endl;
+            exit(0);
+        }
+    }
+}
+
+
+int main (int argc, char **argv)
+{
+    ros::init(argc, argv, "find_object_client");
+
+    ros::NodeHandle nh;
+    ros::Subscriber points = nh.subscribe("/surf/corners_object", 1, paintBox);
+
+    ros::spin();
+    //exit
+    return 0;
 }
--- pan_tilt_object_localization/src/find_object_server.cpp
+++ pan_tilt_object_localization/src/find_object_server.cpp
@@ -1,58 +1,91 @@
 #include <time.h>
 
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <pan_tilt_object_localization/FindAnObjectAction.h>
+#include <pan_tilt_camera_teleop/PanTiltController.h>
+
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
 
-#include "pan_tilt_object_localization/FindAnObject.h"
-#include <pan_tilt_camera_teleop/PanTiltController.h>
+#define SECONDS 2
 
-#define SECONDS 43
-
-PanTiltController _ptc;
-time_t _initTime;
-
-void turnCam()
+class FindAnObjectAction
 {
-  time_t current;
-  
-  while ((time(&current) - _initTime) < SECONDS) 
-  {
-    if((time(&current) % 3) == 0)
-    {
-      _ptc.left();
-      boost::this_thread::sleep(boost::posix_time::milliseconds(50));
-      _ptc.stopPanTilt();
-    }
-  }
-    /*if(time(&current) % 2 == 0) _ptc.left();
-    else _ptc.stopPanTilt();
-  }
-  _ptc.stopPanTilt();
-  system("rosnode kill /surfv");*/
-}
+  protected:
 
-bool searchObject(pan_tilt_object_localization::FindAnObject::Request  &req,
-                  pan_tilt_object_localization::FindAnObject::Response &res)
-{
-  time(&_initTime);
-	ROS_INFO("request: tiempo de busqueda %ld", (long int)req.time);
+  	ros::NodeHandle nh_;
+  	// NodeHandle instance must be created before this line. Otherwise strange error may occur.
+  	actionlib::SimpleActionServer<pan_tilt_object_localization::FindAnObjectAction> as_; 
+  	std::string action_name_;
+  	// create messages that are used to published feedback/result
+  	pan_tilt_object_localization::FindAnObjectFeedback feedback_;
+  	pan_tilt_object_localization::FindAnObjectResult result_;
+  	PanTiltController ptc_;
 
-  boost::thread turn_cam(&turnCam);
-  turn_cam.join();
-	
-	ROS_INFO("sending back response: final de la busqueda");
-  return true;
-}
- 
-int main(int argc, char **argv)
+  public:
+
+  	FindAnObjectAction(std::string name): as_(nh_, name, boost::bind(&FindAnObjectAction::executeCB, this, _1), false), action_name_(name) { as_.start(); }
+
+    ~FindAnObjectAction(void){}
+
+	void turnCam()
+	{
+	  	time_t initTime, current;
+	  	time(&initTime);
+
+		while ((time(&current) - initTime) < SECONDS)
+		{
+	  		ptc_.left();
+	    	boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+	    	ptc_.stopPanTilt();
+	    }
+	}
+
+  	void executeCB(const pan_tilt_object_localization::FindAnObjectGoalConstPtr &goal)
+  	{
+	    // helper variables
+	    ros::Rate r(1);
+	    bool success = false;
+
+	    // push_back the seeds for the fibonacci sequence
+	    feedback_.sequence.clear();
+	    feedback_.sequence.push_back(0);
+	    feedback_.sequence.push_back(1);
+
+	    // publish info to the console for the user
+	    ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->direction, feedback_.sequence[0], feedback_.sequence[1]);
+
+
+	    std::cout << "GOAL = : " << goal->direction << std::endl;
+
+	    if(goal->direction == 1)
+	    {
+	    	turnCam();
+	    	//boost::thread turn_cam(&turnCam);
+	      	//turn_cam.join();
+	      	success = true;
+	    } //td::cout << "Izquierda" << std::endl;
+	    else success = true;
+
+	    if(success)
+	    {
+	      result_.sequence = feedback_.sequence;
+	      ROS_INFO("%s: Succeeded", action_name_.c_str());
+	      // set the action state to succeeded
+	      as_.setSucceeded(result_);
+	    }
+  	}
+
+};
+
+
+int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "find_object");
-  ros::NodeHandle n;
- 
-  ros::ServiceServer service = n.advertiseService("find_time", searchObject);
-  ROS_INFO("Ready to search objects.");
-  ros::spin();
- 
-  return 0;
+	ros::init(argc, argv, "find_object");
+
+	FindAnObjectAction findObject(ros::this_node::getName());
+	ros::spin();
+
+	return 0;
 }
